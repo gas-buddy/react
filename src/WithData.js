78342c72b78786b26a579012f348aa7eeb7f4fe3
Max Metral <max@pyralis.com>
2017-04-27 12:55:18 -0400

    Add WithData component for easy Ajax skullduggery

---
+++ src/WithData.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import PT from 'prop-types';
+import fetch from 'isomorphic-fetch';
+
+/**
+ * This component allows you to easily fetch a server side data payload
+ * and pass it as a property to the child component, along with a loading
+ * boolean
+ */
+export class WithData extends React.Component {
+  state = {
+    loading: false,
+  }
+
+  componentDidMount() {
+    const { url, autoLoad } = this.props;
+    if (url && autoLoad && !('data' in this.state) && !this.state.loading) {
+      this.fetch();
+    }
+  }
+
+  shouldComponentUpdate({ body, url }, nextState) {
+    return (
+      url !== this.props.url ||
+      body !== this.props.body ||
+      nextState !== this.state
+    );
+  }
+
+  componentDidUpdate({ body, url }) {
+    if (this.props.autoLoad && (body !== this.props.body || url !== this.props.url)) {
+      this.fetch();
+    }
+  }
+
+  fetch = () => {
+    const { url, method, body } = this.props;
+    const baseState = { url, method, body };
+
+    fetch(url, {
+      credentials: 'include',
+      method,
+      body: body ? JSON.stringify(body) : undefined,
+    }).then(async (response) => {
+      const data = await response.json();
+      this.setState({
+        ...baseState,
+        data,
+        status: response.status,
+        loading: false,
+      });
+    }).catch((error) => {
+      this.setState({
+        ...baseState,
+        error,
+        status: error.status,
+        loading: false,
+      });
+    });
+    this.setState({
+      ...baseState,
+      loading: true,
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    return children && React.cloneElement(children, {
+      ...this.state,
+      fetch,
+    });
+  }
+}
+
+WithData.propTypes = {
+  children: PT.node,
+  url: PT.string,
+  method: PT.string,
+  // eslint-disable-next-line react/forbid-prop-types
+  body: PT.object,
+  autoLoad: PT.bool,
+};
+
+WithData.defaultProps = {
+  children: null,
+  url: null,
+  method: 'POST',
+  body: null,
+  autoLoad: true,
+};
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export * from './apiMiddleware';
 export * from './store';
 export * from './router';
 export * from './entryPoint';
+export * from './WithData';

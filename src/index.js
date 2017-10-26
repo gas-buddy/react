1eeee6cf8fa7b406f392a118aa7ad0cbc18a6944
Max Metral <max@pyralis.com>
2017-10-26 09:57:45 -0400

    Expose a variety of dependencies directly from @gasbuddy/react to simplify downstream usage and avoid conflicts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import PropTypesModule from 'prop-types';
+
 export * from './apiMiddleware';
 export * from './store';
 export * from './router';
 export * from './entryPoint';
 export * from './WithData';
 export * from './fetchApi';
+
+export { default as React, cloneElement } from 'react';
+export { connect } from 'react-redux';
+export { Link, NavLink, Redirect, withRouter, StaticRouter } from 'react-router-dom';
+export const PT = PropTypesModule;
+export const PropTypes = PropTypesModule;
+export { default as ReactDOMServer, renderToString, renderToStaticMarkup } from 'react-dom/server';
+export { combineReducers } from 'redux';

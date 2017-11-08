613e1f4f8106836d50e31dd0df92172d6b57b20d
Matthew Pavlinsky <mpavlinsky@gmail.com>
2017-11-08 16:25:12 -0500

    Only parse JSON if it says it's JSON.

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -14,7 +14,11 @@ export async function fetchApi(request) {
   })
     .then(async (response) => {
       const { headers, status } = response;
-      const responseBody = await response.json();
+      let responseBody;
+      const contentType = response.headers.get("content-type").toLowerCase();
+      if (contentType && contentType.includes("application/json")) {
+        responseBody = await response.json();
+      }
       return { request, status, headers, body: responseBody };
     });
   if (catchErrors) {

dcaf654face894a464ad7f295901b904add3c9af
Max Metral <max@pyralis.com>
2018-11-23 22:50:15 -0500

    Remove apiMiddleware by using plain store

--- src/minimalEntryPoint.js
+++ src/minimalEntryPoint.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, Provider } from './store';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
 export function minimalEntryPoint({
   reducers,
@@ -12,7 +13,16 @@ export function minimalEntryPoint({
 }) {
   const rootNode = document.querySelector(rootSelector);
   const supportsHistory = 'pushState' in window.history;
-  const store = createStore({ reducers, initialState });
+  let storeCreator = createStore;
+  if (process.env.NODE_ENV !== 'production') {
+    try {
+      // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+      storeCreator = applyMiddleware([require('redux-logger').logger])(storeCreator);
+    } catch (error) {
+      // Nothing to do here...
+    }
+  }
+  const store = storeCreator({ reducers, initialState });
 
   const render = (Component) => {
     ReactDOM.render((
